439e153122a985df93a507d75eb4f2544f786c8d
frekerzzzz <kannamugesh135@gmail.com>
2024-07-20 20:35:52 +0530

    inatial

---
+++ src/SplineComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+const SplineComponent = () => {
+  return (
+    <div style={{ width: '100%', height: '100vh', backgroundColor:"#1c87dd" }}>
+      <Spline scene="https://prod.spline.design/nZadl6wZlcuuJIFR/scene.splinecode" />
+    </div>
+  );
+};
+
+export default SplineComponent;
